847d2585e9c48ecd5d8e38238dbc8ea2b7329b8a
Jerry <fenturechance@gmail.com>
2021-07-24 21:10:51 +0800

    inheritedModel

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:photo_view/photo_view.dart';
+import 'page2.dart';
 void main() {
+  runApp(GlobalData(
+    child: MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => HomePage(),
+        '/page2': (BuildContext context) => Page2(),
+      },
+    ),
+  ));
+}
 
-  runApp(MaterialApp(
-    home: Scaffold(
-          appBar: AppBar(title: Text('image')),
-          body: PhotoView(
-              imageProvider: AssetImage('assets/images/fake250_100.png')))));
+class HomePage extends StatefulWidget {
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('image')),
+        body: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                GlobalData.of(context).title = 'new Title';
+                Navigator.of(context).pushNamed('/page2');
+              },
+              child: Text('page2'),
+            )
+          ],
+        ));
+  }
+}
+
+class GlobalData extends InheritedModel {
+  GlobalData({
+    this.title,
+    Widget? child,
+  }) : super(child: child!);
+
+  String? title;
+
+  static GlobalData of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<GlobalData>()!;
+  }
+
+  @override
+  bool updateShouldNotify(GlobalData old) => title != old.title;
+
+  @override
+  bool updateShouldNotifyDependent(GlobalData oldWidget, Set dependencies) {
+    if (dependencies.contains('title') && oldWidget.title != title) {
+      return true;
+    }
+    return false;
+  }
 }
--- lib/page2.dart
+++ lib/page2.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import 'main.dart';
 
 class Page2 extends StatelessWidget {
   Page2();
   @override
   Widget build(BuildContext context) {
-    final routes = ModalRoute.of(context);
-    if (routes == null) return SizedBox.shrink();
-    Map arguments = routes.settings.arguments as Map;
+    GlobalData g = GlobalData.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('${arguments['title']}'),
+        title: Text('${g.title}'),
       ),
     );
   }
