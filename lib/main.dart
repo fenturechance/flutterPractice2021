dd5aebd424e2f5ce7160fd41e8f96126eb5f4c1d
Jerry <fenturechance@gmail.com>
2021-07-25 23:50:31 +0800

    validator

--- lib/main.dart
+++ lib/main.dart
@@ -13,53 +13,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _inputController = TextEditingController();
-  String onChangeValue = '';
-  String onSubmitValue = '';
-  String buttonClickValue = '';
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String result = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Center(
-          child: Column(
-        children: [
-          TextField(
-            controller: _inputController,
-            onChanged: (value) {
-              setState(() {
-                onChangeValue = value;
-              });
-            },
-            onSubmitted: (value) {
-              setState(() {
-                onSubmitValue = value;
-              });
-            },
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.blue.shade400,
-                border: InputBorder.none,
-                hintText: "請輸入",
-                hintStyle: TextStyle(color: Colors.yellow)),
-          ),
-          GestureDetector(
-              onTap: () {
-                setState(() {
-                  buttonClickValue = _inputController.text;
-                });
-              },
-              child: Container(
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0),
-                child: Text('按鈕'),
-                decoration: BoxDecoration(color: Colors.blue),
-              )),
-          Text('onChange後：${onChangeValue}'),
-          Text('onSubmit後：${onSubmitValue}'),
-          Text('按下按鈕後：${buttonClickValue}'),
-        ],
-      )),
-    ));
+      body: SafeArea(
+          child: Container(
+              padding: EdgeInsets.all(10.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (String? value) {
+                        if (!isWordAndDigit(value!)) {
+                          return '請輸入英文或數字';
+                        }
+                        return null;
+                      },
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        print(_formKey.currentState!.validate());
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            result = '驗證成功';
+                          });
+                        }
+                      },
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 10.0),
+                        padding: EdgeInsets.all(10.0),
+                        child: Text('按鈕'),
+                        decoration: BoxDecoration(color: Colors.blue),
+                      ),
+                    ),
+                    Text('$result')
+                  ],
+                  ),
+              ))),
+    );
+  }
+
+  bool isWordAndDigit(String value) {
+    return RegExp(r"^[A-Za-z0-9_]+$").hasMatch(value);
   }
 }
