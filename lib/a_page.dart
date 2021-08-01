eb30fadba13d8e54714474d02832a217ad188997
Jerry <fenturechance@gmail.com>
2021-08-01 23:08:03 +0800

    send data to a b page

--- lib/a_page.dart
+++ lib/a_page.dart
@@ -9,6 +9,8 @@ class APage extends StatefulWidget {
 }
 
 class _APageState extends State<APage> {
+  TextEditingController controller = TextEditingController();
+  String? textFromB = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +18,40 @@ class _APageState extends State<APage> {
         title: Text('A頁'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text('去B頁'),
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => BPage()));
-          },
-        ),
-      ),
+          child: Column(
+        children: [
+          Row(
+            children: [
+              Text('傳資料去B頁'),
+              Expanded(
+                child: TextField(
+                  controller: controller,
+                ),
+              )
+            ],
+          ),
+          ElevatedButton(
+            child: Text('去B頁'),
+            onPressed: () {
+              gotoBPage(context);
+            },
+          ),
+          Text('從B頁傳來的資料:$textFromB')
+        ],
+      )),
     );
+  }
+
+  void gotoBPage(BuildContext context) async {
+    String? newText;
+    newText = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => BPage(
+                  textInput: controller.text,
+                )));
+    setState(() {
+      textFromB = newText;
+    });
   }
 }
--- lib/b_page.dart
+++ lib/b_page.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 class BPage extends StatefulWidget {
-  const BPage({Key? key}) : super(key: key);
+  final String? textInput;
+  const BPage({Key? key, this.textInput}) : super(key: key);
 
   @override
-  _BPageState createState() => _BPageState();
+  _BPageState createState() => _BPageState(textInput: textInput);
 }
 
 class _BPageState extends State<BPage> {
+  final String? textInput;
+  TextEditingController controller = TextEditingController();
+  _BPageState({this.textInput});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,13 +19,27 @@ class _BPageState extends State<BPage> {
         title: Text('B頁'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text('去上一頁'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
+          child: Column(
+        children: [
+          Text('從A頁傳來的資料: $textInput'),
+          Row(
+            children: [
+              Text('傳資料去A頁'),
+              Expanded(
+                child: TextField(
+                  controller: controller,
+                ),
+              )
+            ],
+          ),
+          ElevatedButton(
+            child: Text('去上一頁'),
+            onPressed: () {
+              Navigator.pop(context, controller.text);
+            },
+          ),
+        ],
+      )),
     );
   }
 }
