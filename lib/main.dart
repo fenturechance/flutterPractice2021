7f35952221372fbc456610f62e7d2298a261ce66
Jerry <fenturechance@gmail.com>
2021-08-05 23:18:21 +0800

    TweenSequenceItem / TweenSequence

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,39 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
-  static const Duration _duration = Duration(seconds: 3);
-  static const String message = loremIpsum;
+  static const Duration duration = Duration(seconds: 3);
   late final AnimationController controller;
-  late final Animation<String> animation;
+  late final Animation<Color?> animation;
 
+  static final colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple,
+  ];
   @override
   void initState() {
     super.initState();
 
-    controller = AnimationController(vsync: this, duration: _duration);
-    animation = TypewriterTween(end: message).animate(controller);
+    final sequenceItems = <TweenSequenceItem<Color?>>[];
+
+    for (var i = 0; i < colors.length; i++) {
+      final beginColor = colors[i];
+      final endColor = colors[(i + 1) % colors.length];
+      final weight = 1 / colors.length;
+
+      sequenceItems.add(
+        TweenSequenceItem<Color?>(
+          tween: ColorTween(begin: beginColor, end: endColor),
+          weight: weight,
+        ),
+      );
+    }
+    controller = AnimationController(duration: duration, vsync: this);
+    animation = TweenSequence<Color?>(sequenceItems).animate(controller);
   }
 
   @override
@@ -39,85 +61,23 @@ class _HomePageState extends State<HomePage>
     return Scaffold(
       appBar: AppBar(
         title: const Text('Custom Tween'),
-        actions: [
-          MaterialButton(
-            child: Text(
-              controller.status == AnimationStatus.completed
-                  ? 'Delete Essay'
-                  : 'Write Essay',
-            ),
-            textColor: Colors.white,
-            onPressed: () {
-              if (controller.status == AnimationStatus.completed) {
-                controller.reverse().whenComplete(() {
-                  setState(() {});
-                });
-              } else {
-                controller.forward().whenComplete(() {
-                  setState(() {});
-                });
-              }
-            },
-          ),
-        ],
       ),
-      body: SafeArea(
-        child: Container(
-          alignment: Alignment.bottomCenter,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    child: Container(
-                      padding: const EdgeInsets.all(8.0),
-                      child: AnimatedBuilder(
-                        animation: animation,
-                        builder: (context, child) {
-                          return Text(
-                            animation.value,
-                            style: const TextStyle(
-                                fontSize: 16, fontFamily: 'SpecialElite'),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: animation,
+          builder: (context, child) {
+            return MaterialButton(
+              color: animation.value,
+              onPressed: () async {
+                await controller.forward();
+                controller.reset();
+              },
+              child: child,
+            );
+          },
+          child: const Text('Animate', style: TextStyle(color: Colors.white)),
         ),
       ),
     );
   }
 }
-
-class TypewriterTween extends Tween<String> {
-  TypewriterTween({String begin = '', String end = ''})
-      : super(begin: begin, end: end);
-
-  @override
-  String lerp(double t) {
-    var cutoff = (end!.length * t).round();
-    return end!.substring(0, cutoff);
-  }
-}
-
-const String loremIpsum = '''
-Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium
-doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore
-veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim
-ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia
-consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque
-porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur
-adipisci[ng] velit, sed quia non-numquam [do] eius modi tempora inci[di]dunt, ut
-labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,
-quis nostrum[d] exercitationem ullam corporis suscipit laboriosam, nisi ut
-aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in
-ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui
-dolorem eum fugiat, quo voluptas nulla pariatur?
-''';
