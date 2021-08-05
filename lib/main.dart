240b18f113b5cd8c5a5bb9ead9aab288cff6ac69
Jerry <fenturechance@gmail.com>
2021-08-05 23:15:41 +0800

    TypewriterTween / MaterialButton / reverse().whenComplete / SingleChildScrollView / substring

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,17 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
-  static const Color beginColor = Colors.deepPurple;
-  static const Color endColor = Colors.deepOrange;
-  Duration duration = const Duration(milliseconds: 800);
-  late AnimationController controller;
-  late Animation<Color?> animation;
+  static const Duration _duration = Duration(seconds: 3);
+  static const String message = loremIpsum;
+  late final AnimationController controller;
+  late final Animation<String> animation;
+
   @override
   void initState() {
     super.initState();
 
-    controller = AnimationController(vsync: this, duration: duration);
-    animation =
-        ColorTween(begin: beginColor, end: endColor).animate(controller);
+    controller = AnimationController(vsync: this, duration: _duration);
+    animation = TypewriterTween(end: message).animate(controller);
   }
 
   @override
@@ -35,37 +34,90 @@ class _HomePageState extends State<HomePage>
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AnimatedBuilder'),
+        title: const Text('Custom Tween'),
+        actions: [
+          MaterialButton(
+            child: Text(
+              controller.status == AnimationStatus.completed
+                  ? 'Delete Essay'
+                  : 'Write Essay',
+            ),
+            textColor: Colors.white,
+            onPressed: () {
+              if (controller.status == AnimationStatus.completed) {
+                controller.reverse().whenComplete(() {
+                  setState(() {});
+                });
+              } else {
+                controller.forward().whenComplete(() {
+                  setState(() {});
+                });
+              }
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: AnimatedBuilder(
-        animation: animation,
-        builder: (context, child) {
-          return MaterialButton(
-            color: animation.value,
-            height: 50,
-            minWidth: 100,
-            child: child,
-              onPressed: () {
-                if (controller.status == AnimationStatus.completed) {
-                  controller.reverse();
-                } else {
-                  controller.forward();
-                }
-              },
-            );
-        },
-        child: const Text(
-          'Change Color',
-          style: TextStyle(color: Colors.white),
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.bottomCenter,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: AnimatedBuilder(
+                        animation: animation,
+                        builder: (context, child) {
+                          return Text(
+                            animation.value,
+                            style: const TextStyle(
+                                fontSize: 16, fontFamily: 'SpecialElite'),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        )
       ),
     );
   }
 }
+
+class TypewriterTween extends Tween<String> {
+  TypewriterTween({String begin = '', String end = ''})
+      : super(begin: begin, end: end);
+
+  @override
+  String lerp(double t) {
+    var cutoff = (end!.length * t).round();
+    return end!.substring(0, cutoff);
+  }
+}
+
+const String loremIpsum = '''
+Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium
+doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore
+veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim
+ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia
+consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque
+porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur
+adipisci[ng] velit, sed quia non-numquam [do] eius modi tempora inci[di]dunt, ut
+labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,
+quis nostrum[d] exercitationem ullam corporis suscipit laboriosam, nisi ut
+aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in
+ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui
+dolorem eum fugiat, quo voluptas nulla pariatur?
+''';
